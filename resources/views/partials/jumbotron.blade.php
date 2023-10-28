4a5280cd0f5defe7eda0009f98a1d697eda45df0
Fabio Costantino <fabiocostantino84@gmail.com>
2023-10-28 17:02:30 +0200

    fixed style jumbotron

--- resources/views/partials/jumbotron.blade.php
+++ resources/views/partials/jumbotron.blade.php
@@ -28,39 +28,9 @@
 </div>
 
 <style>
-    .jumbotron {
-        background-color: $secondary-color;
-    }
+    .imgContainer{
 
-    .imgContainer {
-        height: 450px;
-        background-size: cover;
         background-image: url({{ Vite::asset('resources/img/jumbotron.jpg') }});
     }
-
-    .card {
-        border: none;
-    }
-
-    .imgSeries {
-        height: 200px;
-    }
-
-    .j_btn_top {
-        padding: 1rem;
-        background-color: $primary-color;
-        width: 15%;
-        font-weight: bold;
-        text-align: center;
-        position: relative;
-        top: -25px;
-        left: 350px;
-    }
-
-    .j_btn {
-        padding: 1rem;
-        background-color: $primary-color;
-        width: 20%;
-        font-weight: bold;
-    }
+   
 </style>
